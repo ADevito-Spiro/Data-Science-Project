e1f85bbe65ff164bfe521fe44cb71b430bdb0a48
Austin Devito <aspiro439@gmail.com>
2025-04-08 11:32:52 -0400

    Add data, app setup

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-import pandas as pd, numpy, sklearn
-#pip install pandas
-#pip install scikit-learn
+import pandas as pd, numpy as np, sklearn, matplotlib.pyplot as plt
+from nltk.tokenize import word_tokenize
+"""
+pip install pandas
+pip install scikit-learn
+pip install matplotlib
+pip install nltk
 
+"""
 
+data = pd.read_csv('toxicity_data/test.csv')
+labels = pd.read_csv('toxicity_data/test_labels.csv')
+df = pd.DataFrame(data)
+kf = pd.DataFrame(labels)
 
-def main():
-    print("Hi Marky & Eliy")
+x = df['comment_text']
+y = kf[['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identify_hate']]
 
-if __name__ == '__main__':
-    main()
+print(x)
