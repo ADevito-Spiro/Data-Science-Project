9a93de4214f27456e99330cb24997b0b75f37674
Austin Devito <aspiro439@gmail.com>
2025-04-13 19:12:16 -0400

    Used chat to make accuracy better. Make it seem less chat generated

--- app.py
+++ app.py
@@ -1,24 +1,23 @@
 import pandas as pd, numpy as np, matplotlib.pyplot as plt
-
-#I used two of the homework models in order to do some stuff
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn.pipeline import make_pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
-
-
 
 # Input the CSV data, one of the comments and one for the labels.
 data = pd.read_csv('toxicity_data/train.csv')
 test_labels = pd.read_csv('toxicity_data/test_labels.csv')
 test_data = pd.read_csv('toxicity_data/test.csv')
 df = pd.DataFrame(data)
-kf = pd.DataFrame(test_labels)
-tf = pd.DataFrame(test_data)
+
+# Filter out rows in test_labels where any label is -1
+valid_mask = (test_labels[['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']] != -1).all(axis=1)
+
+test_data = test_data[valid_mask].reset_index(drop=True)
+test_labels = test_labels[valid_mask].reset_index(drop=True)
 
 # Assign the comments as the X and the different determinations of toxicity as the Y
 x = df['comment_text']
-#print(x)
 y = df['toxic']
 z = df['severe_toxic']
 k = df['obscene']
@@ -26,16 +25,16 @@ n = df['threat']
 m = df['insult']
 h = df['identity_hate']
 
-test_x = tf['comment_text']
-test_y = kf['toxic']
-test_z = kf['severe_toxic']
-test_k = kf['obscene']
-test_n = kf['threat']
-test_m = kf['insult']
-test_h = kf['identity_hate']
+test_x = test_data['comment_text']
+test_y = test_labels['toxic']
+test_z = test_labels['severe_toxic']
+test_k = test_labels['obscene']
+test_n = test_labels['threat']
+test_m = test_labels['insult']
+test_h = test_labels['identity_hate']
 
-x_train = x.iloc[:10000]  
-y_train = y.iloc[:10000]  
+x_train = x.iloc[:10000]
+y_train = y.iloc[:10000]
 z_train = z.iloc[:10000]
 k_train = k.iloc[:10000]
 n_train = n.iloc[:10000]
@@ -47,7 +46,7 @@ results = {"Model": [], "Accuracy": []}
 
 for label in label_names:
     y_train = df[label].iloc[:10000]
-    y_test = kf[label].iloc[:10000]
+    y_test = test_labels[label].iloc[:10000]
 
     model = make_pipeline(TfidfVectorizer(ngram_range=(1,2), max_df=0.9, min_df=5, stop_words='english'),
                           LogisticRegression(max_iter=100000, class_weight='balanced')
