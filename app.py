bafd4a02b874c9d1989783bc0d4a58d623969c9c
ESDink <eh13739@georgiasouthern.edu>
2025-04-24 11:44:51 -0400

    Updated Data and Training Space

--- app.py
+++ app.py
@@ -7,15 +7,9 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 # Input the CSV data, one of the comments and one for the labels.
 data = pd.read_csv('toxicity_data/train.csv')
-test_labels = pd.read_csv('toxicity_data/test_labels.csv')
-test_data = pd.read_csv('toxicity_data/test.csv')
+test_labels = pd.read_csv('toxicity_data/test_labels_cleaned.csv')
+test_data = pd.read_csv('toxicity_data/test_filtered.csv')
 df = pd.DataFrame(data)
-
-# Filter out rows in test_labels where any label is -1
-valid_mask = (test_labels[['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']] != -1).all(axis=1)
-
-test_data = test_data[valid_mask].reset_index(drop=True)
-test_labels = test_labels[valid_mask].reset_index(drop=True)
 
 # Assign the comments as the X and the different determinations of toxicity as the Y
 x = df['comment_text']
@@ -34,20 +28,20 @@ test_n = test_labels['threat']
 test_m = test_labels['insult']
 test_h = test_labels['identity_hate']
 
-x_train = x.iloc[:10000]
-y_train = y.iloc[:10000]
-z_train = z.iloc[:10000]
-k_train = k.iloc[:10000]
-n_train = n.iloc[:10000]
-m_train = m.iloc[:10000]
-h_train = h.iloc[:10000]
+x_train = x.iloc[:100000]
+y_train = y.iloc[:100000]
+z_train = z.iloc[:100000]
+k_train = k.iloc[:100000]
+n_train = n.iloc[:100000]
+m_train = m.iloc[:100000]
+h_train = h.iloc[:100000]
 
 label_names = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
 results = {"Model": [], "Accuracy": []}
 
 for label in label_names:
-    y_train = df[label].iloc[:10000]
-    y_test = test_labels[label].iloc[:10000]
+    y_train = df[label].iloc[:100000]
+    y_test = test_labels[label].iloc[:60000]
 
     model = make_pipeline(TfidfVectorizer(),
                           LogisticRegression(max_iter=100000, class_weight='balanced')
